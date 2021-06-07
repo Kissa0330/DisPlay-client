982d5a33cc98cbef0a91b66001f0ace7db06f807
らいか <raika4789@gmail.com>
2021-06-07 11:32:45 +0900

    favicon自動更新関数を追加

--- src/static/js/changeFavicon.js
+++ src/static/js/changeFavicon.js
@@ -14,33 +14,37 @@ const changeFavicon = function () {
     "data:image/svg+xml;charset=utf-8;base64," +
     btoa(unescape(encodeURIComponent(svgData)));
   image.onload = function () {
-    addLink(image.src, "icon");
+    addLink(image.src, "icon", 307);
+    for (let i = 0; i < imagesSizes.length; i++) {
+      let imgSize = imagesSizes[i];
+      console.log(i,imgSize+"pxのfaviconを更新中");
+      changeFavicons(image, imgSize);
+    }
   };
 
-  console.log(imagesSizes.length)
-  for (let i = 0; i < imagesSizes.length; i++) {
-    let imgSize = imagesSizes[i];
-    console.log(i);
-    changeSize(image, imgSize, imgSize);
-    
-    // todo JSONの上書き処理を書き込む
-  }
   let docHead = document.getElementsByTagName("head")[0];
 
-  function addLink(iconURL, relValue) {
+  function addLink(iconURL, relValue, size) {
     let link = document.createElement("link");
     link.type = "image/png";
     link.rel = relValue;
     link.href = iconURL;
-    removeLinkIfExists(relValue);
+    link.sizes = size + "×" + size;
+    removeLinkIfExists(relValue, size);
     docHead.appendChild(link);
   }
 
-  function removeLinkIfExists(relValue) {
+  function removeLinkIfExists(relValue, size) {
     let links = docHead.getElementsByTagName("link");
+    let comparisonSize = size + "×" + size
     for (let i = 0; i < links.length; i++) {
       let link = links[i];
-      if (link.type == "image/png" && link.rel == relValue) {
+      if (
+        link.rel == relValue &&
+        link.sizes == comparisonSize
+      ) {
+        console.log(relValue,link.rel,link.sizes,comparisonSize)
+        console.log("faviconをupdateしました");
         docHead.removeChild(link);
         return;
       }
@@ -52,10 +56,16 @@ const changeFavicon = function () {
     canvas.width = width;
     canvas.height = height;
     let ctx = canvas.getContext("2d");
-    console.log(canvas);
     ctx.drawImage(img, 0, 0, width, height);
     const imgData = canvas.toDataURL("image/png");
-    console.log(imgData);
+    return imgData;
+  }
+
+  function changeFavicons(img, size) {
+    const width = size;
+    const height = size;
+    const imgData = changeSize(img, width, height);
+    addLink(imgData, "icon", size);
   }
 };
 export { changeFavicon };
